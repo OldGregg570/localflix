03b7a8cf68f1c715fa9ecee3a1e31d7d5d64a167
Andrew Bragg <andrew.bragg@autodesk.com>
2016-04-01 17:07:47 -0700

    Holding off on generating thumbnails for now

--- src/server/index.js
+++ src/server/index.js
@@ -6,10 +6,11 @@ let dirTree = require('directory-tree'),
     ffmpeg = require('fluent-ffmpeg'),
     fs = require('fs'),
     Async = require('async'),
-    mkdirp = require('mkdirp');
+    mkdirp = require('mkdirp'),
+    config = require('config');
 
 const EXTENSIONS = require('./extensions.js'),
-      MAIN_SHOWS_DIR = 'D:/tv/videos';
+      MAIN_SHOWS_DIR = config.main_shows_dir;
 
 
 function isFile (c) { return c.type === 'file'; }
@@ -23,6 +24,7 @@ module.exports = {
 
 
   tree.children.filter(isDir).forEach((dir) => {
+   console.log(dir.path);
    thumbnailPromises.push(getThumbnails(dir));
   });
 
@@ -55,7 +57,7 @@ module.exports = {
 
 
   function createThumbnail (show) {
-   const SERVED_DIR = 'D:/tv/src/public/';
+   const SERVED_DIR = path.join(__dirname, '/../', 'public');
    var parsedPath = path.parse(show.path),
        thumbnailFolder = path.join('thumbnails', p, parsedPath.dir, parsedPath.base.split('.').join('')),
        options = { count: 1, timemarks: [ '10' ], folder: path.join(SERVED_DIR, thumbnailFolder) },
@@ -63,15 +65,18 @@ module.exports = {
 
    return new Promise (function (resolve) {
     mkdirp (path.join (SERVED_DIR, thumbnailFolder), function (err) {
-      var fpath;
-      ffmpeg(videoPath)
-         .on('filenames', (filenames) => {
-          fpath = path.join(thumbnailFolder, filenames[0]);
-          show.thumbnail = fpath;
-         })
-         .on('end', (err) => { resolve (fpath); })
-         .screenshots(options);
-     });
+      // fs.writeFile(SERVED_DIR + thumbnailFolder + 'tn.png', {}, function (err) {
+      //  console.log(err);
+      // });
+
+     //  ffmpeg(videoPath)
+     //     .on('filenames', (filenames) => {
+     //      fpath = path.join(thumbnailFolder, filenames[0]);
+     //      show.thumbnail = fpath;
+     //     })
+     //     .on('end', (err) => { resolve (fpath); })
+     //     .screenshots(options);
+     // });
     });
   }
  }
